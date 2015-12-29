2dec68047b688058fc0a9500d453809c609a6ae5
sean <sean.cox.or@gmail.com>
2015-12-29 15:12:53 -0800

    initial commit

---
+++ web/anastasia.js
@@ -0,0 +1,31 @@
+var images1 = [
+  'http://terezowens.com/wp-content/uploads/2014/07/anastasia-revolve-5.jpg',
+  'https://cdn1.lockerdome.com/uploads/17cf2f82a4afad56d2cdbe6381f7852683f03bf0c81b2cfba4a2bffe62f79c16_large',
+  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTfUbUCfw8SYljm8RHM7DeD2J5q7Dyb7ib537jpCKWWt82Qi426',
+  'http://anastasiaashley.com/wp-content/uploads/2015/07/Hot-for-hammock-001.jpeg',
+  'https://coedmagazine.files.wordpress.com/2014/12/maja-krag.jpg',
+  'http://i0.wp.com/www.brobible.com/wp-content/uploads/2014/07/Anastasia-Ashley-4.jpg?resize=550%2C550',
+  'https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQqHJMI_dpmQ5DyH0qwwmesCqY2KPAn6PyGs_OIpLwIqUk8vdd0',
+  'http://s1.totalprosports.com/wp-content/uploads/2014/12/6-Anastasia-Ashley-Instagram-hottest-female-athletes-of-2014.jpg',
+  'http://static.bongdaplus.vn/Assets/Media/2013/12/30/8//anastasia12.jpg',
+  'http://cdn-jpg.si.com/sites/default/files/styles/si_article_main/public/images/anastasia-ashley-galore-lede.jpg?itok=yIujUi5V'
+]
+
+var images2 = [
+  'http://cultureandlife.files.wordpress.com/2014/02/sports-illustrated-swimsuit-edition-2014_08.jpg',
+  'http://cdn-jpg.si.com/sites/default/files/styles/inline_gallery_desktop/public/2013/06/bar5.jpg?itok=RNNJkzz5',
+  'http://i1.wp.com/scontent-a.cdninstagram.com/hphotos-xfa1/t51.2885-15/e15/10953614_1407436566218673_1602089445_n.jpg?w=474',
+  'http://i1321.photobucket.com/albums/u541/mikeyisawesome1/ashley4_zps91bed600.jpg?t=1392835957',
+  'http://www.extremeelite.com/wp-content/uploads/2014/08/anastasia-ashley-revolve-babes-of-the-summer-campaign-featured.jpg',
+  'https://pbs.twimg.com/media/BsDmTkXCEAEHffE.jpg:large',
+  'http://i0.wp.com/www.brobible.com/wp-content/uploads/2015/05/anastasia_ashley_photoshoot_3.jpg?resize=640%2C958',
+  'https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcSztaj33bjMquoseAC0hmZikm3uG25-OmmD7fG92ZX1mOd6FP5ysA',
+
+]
+var options = {
+  duration: 3000,
+  fade: 900
+}
+
+$('#slider1').backstretch(images1, options)
+$('#slider2').backstretch(images2, options)
